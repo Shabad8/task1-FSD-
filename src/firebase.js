118dbeaa5f9b15d3354bcac36e4f0bbf7c93e457
Shabad8 <shabad4911.se24@chitkara.edu.in>
2025-10-13 22:48:23 +0530

    Initial commit for Task 7.1P

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.YOUR_APP_PREFIX_FIREBASE_API_KEY || process.env.YOUR_APP_PREFIX_FIREBASE_API_KEY,
-  authDomain: import.meta.env.YOUR_APP_PREFIX_FIREBASE_AUTH_DOMAIN || process.env.YOUR_APP_PREFIX_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.YOUR_APP_PREFIX_FIREBASE_PROJECT_ID || process.env.YOUR_APP_PREFIX_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.YOUR_APP_PREFIX_FIREBASE_STORAGE_BUCKET || process.env.YOUR_APP_PREFIX_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.YOUR_APP_PREFIX_FIREBASE_MESSAGING_SENDER_ID || process.env.YOUR_APP_PREFIX_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.YOUR_APP_PREFIX_FIREBASE_APP_ID || process.env.YOUR_APP_PREFIX_FIREBASE_APP_ID,
-  measurementId: import.meta.env.YOUR_APP_PREFIX_FIREBASE_MEASUREMENT_ID || process.env.YOUR_APP_PREFIX_FIREBASE_MEASUREMENT_ID
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
